8b7252e6b9ad10a66c9d9020eef55a967cf5c28f
git-ebetts <ebettsmail@gmail.com>
2015-06-03 08:58:33 -0400

    myitem search functional

--- app/app.js
+++ app/app.js
@@ -9,6 +9,7 @@ stuffExchApp.config(["$routeProvider", function($routeProvider) {
 	$routeProvider
 	.when("/",  { controller: "AddItemController", templateUrl: "app/partials/signin_signup.html"  })
 	.when("/items",  { controller: "ListController", templateUrl: "app/partials/list_partial.html" })
+	.when("/myitems",  { controller: "ListController", templateUrl: "app/partials/list_partial2.html" })
 	.when("/login",  { controller: "LoginController", templateUrl: "app/partials/loginform.html" })
 	.when("/register",  { controller: "LoginController", templateUrl: "app/partials/registerform.html" })
 	.when("/signin_signup",  { controller: "AddItemController", templateUrl: "app/partials/signin_signup.html"  })
